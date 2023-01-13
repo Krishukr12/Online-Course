5d934c638134acf1d7eac6d8bddc8f415f0272ed
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-01-13 22:26:40 +0530

    PaymentFail Page : Fixing bug with Functionality

--- online_course_frontend/src/Pages/Payment/PaymentFail.jsx
+++ online_course_frontend/src/Pages/Payment/PaymentFail.jsx
@@ -2,6 +2,7 @@ import { Button, Container, Heading, VStack } from "@chakra-ui/react";
 import React from "react";
 import { RiErrorWarningFill } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { BsArrowRepeat } from "react-icons/bs";
 export const PaymentFail = () => {
   return (
     <Container h="85vh">
@@ -9,7 +10,13 @@ export const PaymentFail = () => {
         <RiErrorWarningFill size={"5rem"} />
         <Heading textTransform={"uppercase"}>Payment Fail !</Heading>
         <Link to="/subscribe">
-          <Button>Try Again</Button>
+          <Button
+            variant="outline"
+            colorScheme="teal"
+            rightIcon={<BsArrowRepeat />}
+          >
+            Try Again
+          </Button>
         </Link>
       </VStack>
     </Container>
